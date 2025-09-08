f2f945f5147364c03fdef47f45ee50e5c435f9a1
Bastian Brisso <166576401+Brissox@users.noreply.github.com>
2025-09-08 16:53:49 +0000

    se agregan a carpetas separadas

--- js/utils/productos.js
+++ js/utils/productos.js
@@ -4,175 +4,175 @@ let productos = [
     precio: 45000,
     descripcion: "Deliciosa torta de chocolate con capas de ganache y un toque de avellanas. Personalizable con mensajes especiales.",
     categoria: "Tortas Cuadradas",
-    imagen: "assets/img/productos/todo/torta_cuadrada_choc.jpg"
+    imagen: "assets/img/productos/tortas/torta_cuadrada_choc.jpg"
   },
   {
     nombre: "Torta Cuadrada de Frutas",
     precio: 50000,
     descripcion: "Una mezcla de frutas frescas y crema chantilly sobre un suave bizcocho de vainilla, ideal para celebraciones.",
     categoria: "Tortas Cuadradas",
-    imagen: "assets/img/productos/todo/torta_frutas.jpg"
+    imagen: "assets/img/productos/tortas/torta_frutas.jpg"
   },
   {
     nombre: "Torta Circular de Vainilla",
     precio: 40000,
     descripcion: "Bizcocho de vainilla clásico relleno con crema pastelera y cubierto con un glaseado dulce, perfecto para cualquier ocasión.",
     categoria: "Tortas Circulares",
-    imagen: "assets/img/productos/todo/Bizcocho-vainilla-2024.png"
+    imagen: "assets/img/productos/tortas/Bizcocho-vainilla-2024.png"
   },
   {
     nombre: "Torta Circular de Manjar",
     precio: 42000,
     descripcion: "Torta tradicional chilena con manjar y nueces, un deleite para los amantes de los sabores dulces y clásicos.",
     categoria: "Tortas Circulares",
-    imagen: "assets/img/productos/todo/Bizcocho-nuez-2024.png"
+    imagen: "assets/img/productos/tortas/Bizcocho-nuez-2024.png"
   },
   {
     nombre: "Mousse de Chocolate",
     precio: 5000,
     descripcion: "Postre individual cremoso y suave, hecho con chocolate de alta calidad, ideal para los amantes del chocolate.",
     categoria: "Postres Individuales",
-    imagen: "assets/img/productos/todo/mousse-de-chocolate.jpg"
+    imagen: "assets/img/productos/postres/mousse-de-chocolate.jpg"
   },
   {
     nombre: "Tiramisú Clásico",
     precio: 5500,
     descripcion: "Un postre italiano individual con capas de café, mascarpone y cacao, perfecto para finalizar cualquier comida.",
     categoria: "Postres Individuales",
-    imagen: "assets/img/productos/todo/Tiramis-en-frascos.jpg"
+    imagen: "assets/img/productos/postres/Tiramis-en-frascos.jpg"
   },
   {
     nombre: "Torta Sin Azúcar de Naranja",
     precio: 48000,
     descripcion: "Torta ligera y deliciosa, endulzada naturalmente, ideal para quienes buscan opciones más saludables.",
     categoria: "Productos Sin Azúcar",
-    imagen: "assets/img/productos/todo/Panqueque-naranja.jpg"
+    imagen: "assets/img/productos/tortas/Panqueque-naranja.jpg"
   },
   {
     nombre: "Cheesecake Sin Azúcar",
     precio: 47000,
     descripcion: "Suave y cremoso, este cheesecake es una opción perfecta para disfrutar sin culpa.",
     categoria: "Productos Sin Azúcar",
-    imagen: "assets/img/productos/todo/Cheesecake frutos del bosque.jpg"
+    imagen: "assets/img/productos/pasteles/Cheesecake frutos del bosque.jpg"
   },
   {
     nombre: "Empanada de Manzana",
     precio: 3000,
     descripcion: "Pastelería tradicional rellena de manzanas especiadas, perfecta para un dulce desayuno o merienda.",
     categoria: "Pastelería Tradicional",
-    imagen: "assets/img/productos/todo/Empanadillas-de-manzana.jpg"
+    imagen: "assets/img/productos/otros/Empanadillas-de-manzana.jpg"
   },
   {
     nombre: "Tarta de Santiago",
     precio: 6000,
     descripcion: "Tradicional tarta española hecha con almendras, azúcar, y huevos, una delicia para los amantes de los postres clásicos.",
     categoria: "Pastelería Tradicional",
-    imagen: "assets/img/productos/todo/tarta-santiago.jpg"
+    imagen: "assets/img/productos/pasteles/tarta-santiago.jpg"
   },
   {
     nombre: "Brownie Sin Gluten",
     precio: 4000,
     descripcion: "Rico y denso, este brownie es perfecto para quienes necesitan evitar el gluten sin sacrificar el sabor.",
     categoria: "Productos Sin Gluten",
-    imagen: "assets/img/productos/todo/brownie.jpg"
+    imagen: "assets/img/productos/pasteles/brownie.jpg"
   },
   {
     nombre: "Pan Sin Gluten",
     precio: 3500,
     descripcion: "Suave y esponjoso, ideal para sándwiches o para acompañar cualquier comida.",
     categoria: "Productos Sin Gluten",
-    imagen: "assets/img/productos/todo/pan-de-molde-sin-gluten-casero.jpg"
+    imagen: "assets/img/productos/otros/pan-de-molde-sin-gluten-casero.jpg"
   },
   {
     nombre: "Torta Vegana de Chocolate",
     precio: 50000,
     descripcion: "Torta de chocolate húmeda y deliciosa, hecha sin productos de origen animal, perfecta para veganos.",
     categoria: "Productos Vegana",
-    imagen: "assets/img/productos/todo/Marmolada-2024-600x600.png"
+    imagen: "assets/img/productos/tortas/Marmolada-2024-600x600.png"
   },
   {
     nombre: "Galletas Veganas de Avena",
     precio: 4500,
     descripcion: "Crujientes y sabrosas, estas galletas son una excelente opción para un snack saludable y vegano.",
     categoria: "Productos Vegana",
-    imagen: "assets/img/productos/todo/galletas-de-avena-veganas.jpg"
+    imagen: "assets/img/productos/otros/galletas-de-avena-veganas.jpg"
   },
   {
     nombre: "Torta Especial de Cumpleaños 1",
     precio: 55000,
     descripcion: "Diseñada especialmente para celebraciones, personalizable con decoraciones y mensajes únicos.",
     categoria: "Tortas Especiales",
-    imagen: "assets/img/productos/todo/cu1.jpg"
+    imagen: "assets/img/productos/tortas/cu1.jpg"
   },
   {
     nombre: "Torta Especial de Cumpleaños 2",
     precio: 55000,
     descripcion: "Diseñada especialmente para celebraciones, personalizable con decoraciones y mensajes únicos.",
     categoria: "Tortas Especiales",
-    imagen: "assets/img/productos/todo/cu2.jpg"
+    imagen: "assets/img/productos/tortas/cu2.jpg"
   },
   {
     nombre: "Torta Especial de Cumpleaños 3",
     precio: 55000,
     descripcion: "Diseñada especialmente para celebraciones, personalizable con decoraciones y mensajes únicos.",
     categoria: "Tortas Especiales",
-    imagen: "assets/img/productos/todo/cu3.jpg"
+    imagen: "assets/img/productos/tortas/cu3.jpg"
   },
   {
     nombre: "Torta Especial de Cumpleaños 4",
     precio: 55000,
     descripcion: "Diseñada especialmente para celebraciones, personalizable con decoraciones y mensajes únicos.",
     categoria: "Tortas Especiales",
-    imagen: "assets/img/productos/todo/cu4.jpg"
+    imagen: "assets/img/productos/tortas/cu4.jpg"
   },
   {
     nombre: "Torta Especial de Cumpleaños 5",
     precio: 55000,
     descripcion: "Diseñada especialmente para celebraciones, personalizable con decoraciones y mensajes únicos.",
     categoria: "Tortas Especiales",
-    imagen: "assets/img/productos/todo/cu5.jpg"
+    imagen: "assets/img/productos/tortas/cu5.jpg"
   },
   {
     nombre: "Torta Especial de Cumpleaños 6",
     precio: 55000,
     descripcion: "Diseñada especialmente para celebraciones, personalizable con decoraciones y mensajes únicos.",
     categoria: "Tortas Especiales",
-    imagen: "assets/img/productos/todo/cu10.jpeg"
+    imagen: "assets/img/productos/tortas/cu10.jpeg"
   },
   {
     nombre: "Torta Especial de Cumpleaños 7",
     precio: 55000,
     descripcion: "Diseñada especialmente para celebraciones, personalizable con decoraciones y mensajes únicos.",
     categoria: "Tortas Especiales",
-    imagen: "assets/img/productos/todo/cu7.jpg"
+    imagen: "assets/img/productos/tortas/cu7.jpg"
   },
    {
     nombre: "Torta Especial de Cumpleaños 8",
     precio: 55000,
     descripcion: "Diseñada especialmente para celebraciones, personalizable con decoraciones y mensajes únicos.",
     categoria: "Tortas Especiales",
-    imagen: "assets/img/productos/todo/cu8.jpg"
+    imagen: "assets/img/productos/tortas/cu8.jpg"
   },
    {
     nombre: "Torta Especial de Cumpleaños 9",
     precio: 55000,
     descripcion: "Diseñada especialmente para celebraciones, personalizable con decoraciones y mensajes únicos.",
     categoria: "Tortas Especiales",
-    imagen: "assets/img/productos/todo/cu9.png"
+    imagen: "assets/img/productos/tortas/cu9.png"
   },
   {
     nombre: "Torta Especial de Boda 1",
     precio: 60000,
     descripcion: "Elegante y deliciosa, esta torta está diseñada para ser el centro de atención en cualquier boda.",
     categoria: "Tortas Especiales",
-    imagen: "assets/img/productos/todo/torta_novio1.jpg"
+    imagen: "assets/img/productos/tortas/torta_novio1.jpg"
   },
    {
     nombre: "Torta Especial de Boda 2",
     precio: 60000,
     descripcion: "Elegante y deliciosa, esta torta está diseñada para ser el centro de atención en cualquier boda.",
     categoria: "Tortas Especiales",
-    imagen: "assets/img/productos/todo/torta_novios3.jpg"
+    imagen: "assets/img/productos/tortas/torta_novios3.jpg"
   }
 
 
